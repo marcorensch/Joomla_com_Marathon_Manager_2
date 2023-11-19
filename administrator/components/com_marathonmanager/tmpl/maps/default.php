9891d98699fa2d08337c3ee4e49bafc5664e5f4c
Marco Rensch <marco.rensch@nx-designs.ch>
2023-11-19 14:15:51 +0100

    Backend List Sorting added

--- administrator/components/com_marathonmanager/tmpl/maps/default.php
+++ administrator/components/com_marathonmanager/tmpl/maps/default.php
@@ -61,14 +61,14 @@ if ($saveOrder && !empty($this->items)) {
                                 <?php echo TEXT::_('JSTATUS'); ?>
                             </th>
                             <th scope="col" style="min-width: 150px" class="d-none d-md-table-cell">
-                                <?php echo Text::_('COM_MARATHONMANAGER_TABLE_TABLEHEAD_TITLE'); ?>
+                                <?php echo HTMLHelper::_('searchtools.sort', 'COM_MARATHONMANAGER_TABLE_TABLEHEAD_TITLE', 'a.title', $listDirn, $listOrder); ?>
                             </th>
 
                             <th scope="col" style="width: 10%" class="d-none d-md-table-cell">
                                 <?php echo Text::_('JGRID_HEADING_ACCESS'); ?>
                             </th>
                             <th scope="col" style="">
-                                <?php echo Text::_('COM_MARATHONMANAGER_TABLE_TABLEHEAD_ID'); ?>
+                                <?php echo HTMLHelper::_('searchtools.sort', 'COM_MARATHONMANAGER_TABLE_TABLEHEAD_ID', 'a.id', $listDirn, $listOrder); ?>
                             </th>
                         </tr>
                         </thead>
