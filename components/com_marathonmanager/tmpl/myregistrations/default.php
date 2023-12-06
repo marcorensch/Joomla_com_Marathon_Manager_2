7d1cd8d4bdc2e064f77780c2670ce2202824f327
Marco Rensch <marco.rensch@nx-designs.ch>
2023-12-06 11:51:13 +0100

    Added Animation for My registration elements

--- components/com_marathonmanager/tmpl/myregistrations/default.php
+++ components/com_marathonmanager/tmpl/myregistrations/default.php
@@ -10,11 +10,46 @@
 
 use Joomla\CMS\Factory;
 use Joomla\CMS\HTML\HTMLHelper;
+use Joomla\CMS\Language\Text;
 use Joomla\CMS\Router\Route;
 
 \defined('_JEXEC') or die;
 
 $wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+$wa->useStyle('com_marathonmanager.my-registrations-css');
+$wa->useScript('com_marathonmanager.my-registrations-js');
 ?>
-
-HELLO My REgistrations
+<div class="uk-container" uk-margin uk-grid uk-scrollspy="target:>div; cls: uk-animation-slide-bottom; delay: 50;">
+    <?php
+    for ($i = 0; $i < 4; $i++):
+        ?>
+        <div class="uk-width-1-1">
+            <div class="uk-card uk-card-default registration-card">
+                <div class="uk-card-header nxd-registration-header">
+                    <div class="uk-grid-small uk-flex uk-flex-middle">
+                        <div class="uk-width-auto">
+                            <img class="uk-border-circle nxd-event-registration-image" src="https://picsum.photos/60/60"
+                                 alt="">
+                        </div>
+                        <div class="uk-width-expand">
+                            <h3 class="uk-margin-remove-bottom">Event Title</h3>
+                            <span class="uk-text-meta">12. - 14.12.2023</span>
+                        </div>
+                        <div class="uk-width-auto">
+                            <?php if ($i % 2 === 0): ?>
+                            <span uk-icon="icon: check; ratio:2" class="uk-text-success" uk-tooltip="<?php echo Text::_("COM_MARATHONMANAGER_INFO_PAYMENT_REGISTERED");?>"></span>
+                            <?php else: ?>
+                            <span class="uk-text-warning pulsing" uk-icon="icon: clock; ratio:2" uk-tooltip="<?php echo Text::_("COM_MARATHONMANAGER_INFO_PAYMENT_NOT_REGISTERED");?>"></span>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="nxd-registration-details uk-overflow-hidden">
+                    <div class="uk-card-body ">
+                        Foo
+                    </div>
+                </div>
+            </div>
+        </div>
+    <?php endfor; ?>
+</div>
---
+++ media/com_marathonmanager/js/myregistrations.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM loaded')
+    const registrations = document.querySelectorAll('.registration-card');
+    registrations.forEach(function (registration) {
+        const registrationHeader = registration.querySelector('.nxd-registration-header');
+        const registrationDetails = registration.querySelector('.nxd-registration-details');
+        // Set height to 0 of registration details
+        registrationDetails.style.height = '0px';
+
+        registrationHeader.addEventListener('click', function () {
+            if (registrationDetails.style.height === '0px') {
+                // Animates the height of the registration details
+                registrationDetails.animate([
+                    { height: '0px' },
+                    { height: registrationDetails.scrollHeight + 'px' }
+                ], {
+                    duration: 300,
+                    easing: 'ease-in-out'
+                });
+                registrationDetails.style.height = registrationDetails.scrollHeight + 'px';
+            } else {
+                // Animates the height of the registration details
+                registrationDetails.animate([
+                    { height: registrationDetails.scrollHeight + 'px' },
+                    { height: '0px' }
+                ], {
+                    duration: 300,
+                    easing: 'ease-in-out'
+                });
+                registrationDetails.style.height = '0px';
+            }
+        });
+    });
+});
