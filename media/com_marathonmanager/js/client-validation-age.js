5df937f69a76c820cf513ab7dadc7b3656ee79a0
Marco <marco.rensch@nx-designs.ch>
2024-09-20 05:14:26 +0200

    Console Logs entfernt

--- media/com_marathonmanager/js/client-validation-age.js
+++ media/com_marathonmanager/js/client-validation-age.js
@@ -1,12 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('client-validation-age.js loaded');
     'use strict';
     // OnKeyUp event for the age field
 
     if(document.formvalidator){
-        console.log('client-validation-age.js: formvalidator found');
         document.formvalidator.setHandler('age', function(value, element) {
-            console.log('client-validation-age.js: formvalidator age handler');
             const regex = /^\d{4}$/;
             if(!regex.test(value)){
                 element.dataset.validationText = Joomla.Text._("COM_MARATHONMANAGER_FIELD_BIRTHYEAR_INVALID_FORMAT");
