b7c50c871e7d0405c921bc49f38168745d48aaa3
Marco <marco.rensch@nx-designs.ch>
2024-10-19 09:23:07 +0200

    Newsletter Field Workaround because we cannot access Newsletter List

--- administrator/components/com_marathonmanager/src/Field/AcyMailingListSelectionField.php
+++ administrator/components/com_marathonmanager/src/Field/AcyMailingListSelectionField.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * @package     Joomla.Administrator
+ * @package      Joomla.Administrator
  *              Joomla.Site
- * @subpackage  com_marathonmanager
- * @author      Marco Rensch
+ * @subpackage   com_marathonmanager
+ * @author       Marco Rensch
  * @since        1.0.0
  *
- * @license     GNU General Public License version 2 or later; see LICENSE.txt
- * @copyright   Copyright (C) 2022 nx-designs NXD
+ * @license      GNU General Public License version 2 or later; see LICENSE.txt
+ * @copyright    Copyright (C) 2022 nx-designs NXD
  *
  */
 
@@ -18,16 +18,7 @@ use Joomla\CMS\Form\Field\ListField;
 use Joomla\CMS\HTML\HTMLHelper;
 use Joomla\CMS\Language\Text;
 use Joomla\CMS\Form\FormHelper;
-
-$ds = DIRECTORY_SEPARATOR;
-$acyHelper = rtrim(JPATH_ADMINISTRATOR, $ds).$ds.'components'.$ds.'com_acym'.$ds.'helpers'.$ds.'helper.php';
-if (!include_once($acyHelper)) {
-	$msg =  'COM_MARATHONMANMANAGER_FIELD_ACYM_NOT_FOUND_ERROR';
-	Factory::getApplication()->enqueueMessage(Text::_($msg), 'warning');
-	return false;
-}
-
-use AcyMailing\Classes\ListClass;
+use Joomla\Database\DatabaseInterface;
 
 defined('_JEXEC') or die;
 
@@ -36,24 +27,56 @@ FormHelper::loadFieldClass('list');
 class AcyMailingListSelectionField extends ListField
 {
 
-    protected $type = 'AcyMailingListSelection';
+	protected $type = 'AcyMailingListSelection';
 
-    protected function getOptions(): array
-    {
-        $options = [];
-        $options[] = HTMLHelper::_('select.option', '', Text::_('COM_MARATHONMANAGER_FIELD_DEFAULT_SELECT_ACYMAILING_LIST'));
+	protected function getOptions(): array
+	{
+		$options   = [];
+		$options[] = HTMLHelper::_('select.option', '', Text::_('COM_MARATHONMANAGER_FIELD_DEFAULT_SELECT_ACYMAILING_LIST'));
 
-        $listClass = new ListClass;
-        $allLists = $listClass->getAll();
+//		Is currently not working, because the ListClass is not available idk.
+//        $listClass = new ListClass;
+//        $allLists = $listClass->getAll();
+		$allLists = $this->getAcymLists(true);
 
-        if (!empty($allLists)) {
-            foreach ($allLists as $option) {
-                $options[] = HTMLHelper::_('select.option', $option->id, $option->name);
-            }
-        }
+		if (!empty($allLists))
+		{
+			foreach ($allLists as $option)
+			{
+				$options[] = HTMLHelper::_('select.option', $option->id, $option->name);
+			}
+		}
 
-        return array_merge(parent::getOptions(), $options);
+		return array_merge(parent::getOptions(), $options);
 
-    }
+	}
+
+	private function getAcymLists($onlyActive = false): array
+	{
+		$db    = Factory::getContainer()->get(DatabaseInterface::class);
+		$query = $db->getQuery(true)
+			->select('id, name')
+			->from('#__acym_list');
+
+		if ($onlyActive)
+		{
+			$query->where('active = 1');
+		}
+
+		$db->setQuery($query);
+		try
+		{
+			$lists = $db->loadObjectList();
+		}
+		catch (\Exception $e)
+		{
+			error_log('MarathonManager 2 AcyMailing Integration Error from Selection Field: ' . $e->getMessage());
+			Factory::getApplication()->enqueueMessage(Text::_('COM_MARATHONMANMANAGER_FIELD_ACYM_NOT_FOUND_ERROR'), 'warning');
+			$lists = [];
+		}
+
+		return $lists;
+
+	}
 
 }
